ae5a20ec7389617320ca55c28a1b28cdb64c8028
Tommy Mynn Son <tommy.m.son@gmail.com>
2022-03-10 01:31:12 -0800

    Set up Route components in Switch (Routes) component

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes as Switch, Route, Navigate as Redirect } from 'react-router-dom';
+
+import Results from './Results';
 
 const Routes = () => {
+  const routesThatRenderResults = ['/search', '/images', '/news', 'videos'];
+
   return (
-    <div>
-      <h1>Routes</h1>
+    <div className='p-4'>
+      <Switch>
+        <Route exact path='/' element={<Redirect to='search' />}>
+        </Route>
+        {routesThatRenderResults.map(route => <Route key={route} exact path={route} element={<Results />}></Route>)}
+      </Switch>
     </div>
   );
 };
